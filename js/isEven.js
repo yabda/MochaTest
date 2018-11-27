7b9ab8bacdd814c62df622b7ecae4238092bdafc
yanis <yanis.abdallah@insa-cvl.fr>
2018-11-27 15:51:22 +0100

    update

--- js/isEven.js
+++ js/isEven.js
@@ -1,6 +1,6 @@
 module.exports = function (number){
 	if (number % 1 !== 0)
-		throw new Error("invalid input, not a integer");
+		throw new Error("invalid input, not a integer"); 
 	else
 		return (number%2==0);
 }
